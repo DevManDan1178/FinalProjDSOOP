4f7e8ea1bed02fed5cc2414f47fd0c0bc2c746fb
DevManDan1178 <l3r0d4niel@gmail.com>
2025-04-20 17:49:13 -0400

    Deliv1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,6 @@ import java.time.temporal.TemporalField;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime soon = LocalDateTime.now().minusDays(1);
-        System.out.println(now.getDayOfYear() - soon.getDayOfYear());
-
     }
 
 }
