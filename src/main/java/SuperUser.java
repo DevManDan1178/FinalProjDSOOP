5757a64297e89684ac4910cb95f1701ea77c1bd0
DevManDan1178 <l3r0d4niel@gmail.com>
2025-04-29 20:23:13 -0400

    50% implementation - Deliv2 Added Modifications.txt for new changes to the project and its structure
    Added new SuperUser.java class (incomplete) from the new modifications that will be implemented

---
+++ src/main/java/SuperUser.java
@@ -0,0 +1,84 @@
+import java.security.PublicKey;
+import java.util.Objects;
+
+public abstract class SuperUser {
+    private String name;
+    private int id;
+    private static int currentId;
+
+    public SuperUser(String name) {
+        this.name = name;
+        this.id = currentId++;
+    }
+
+    public SuperUser(String name, int id) {
+        this.name = name;
+        this.id = id;
+        //if the current ID is less than this ID, offset the current ID to bigger so that future SuperUser objects will not have same ID
+        if (currentId < this.id) {
+            currentId = this.id + 1;
+        }
+    }
+
+    /**
+     * Returns a SuperUser from the export data
+     * @param exportData export data
+     * @return a User or Admin with corresponding data members
+     */
+    public SuperUser fromExportData(String exportData) {
+        String[] data = exportData.split(",");
+        if (data.length != 2) {
+            System.out.println("Invalid export data - " + exportData);
+            return null;
+        }
+        return null; //TODO
+    }
+
+    /**
+     * Gets the CSV format of the data fields
+     *
+     * @return CSV format of data
+     */
+    public String getExportData() {
+        return String.format("%s,%s", name, id);
+    }
+
+    @Override
+    public String toString() {
+        return "SuperUser{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SuperUser superUser = (SuperUser) o;
+        return id == superUser.id && Objects.equals(name, superUser.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
